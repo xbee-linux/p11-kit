6d6874ac50013fe76f378e7babd647a0edc13764
Eric FISCHER <efischer78@yahoo.fr>
2022-03-15 19:45:26 +0100

    ok

---
+++ resources/prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+set -e
+
+sed '20,$ d' -i trust/trust-extract-compat &&
+cat >> trust/trust-extract-compat << "EOF"
+# Copy existing anchor modifications to /etc/ssl/local
+/usr/libexec/make-ca/copy-trust-modifications
+
+# Update trust stores
+/usr/sbin/make-ca -r
+EOF
